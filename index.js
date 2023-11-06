89b64a72fe6b1a7fd822b7da5b2bcbaca088d9c0
izaz10921 <izaz10921@gmail.com>
2023-11-06 15:08:54 +0600

    Home page data loaded successfully

--- index.js
+++ index.js
@@ -29,6 +29,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('FoodHub').collection('allFoods');
+
+    app.get('/allFoods',async(req,res) =>{
+
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
